06c5baf0bce8174b50129f57dfe17c4ca665400c
Steve Phillips <steve@tryingtobeawesome.com>
2015-11-15 02:00:52 -0800

    homepage: Added first code examples + output!

--- app.py
+++ app.py
@@ -4,7 +4,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    examples = [
+        {
+            'title': 'descriptivism.py',
+            'embed_url': 'github.com/elimisteve/exphilorg/blob/master/templates/code/descriptivism.py',
+            'output': render_template('code/descriptivism.out'),
+        },
+        {
+            'title': 'direct_reference.py',
+            'embed_url': 'github.com/elimisteve/exphilorg/blob/master/templates/code/direct_reference.py',
+            'output': render_template('code/direct_reference.out'),
+        },
+        {
+            'title': 'voting.py',
+            'embed_url': 'github.com/elimisteve/exphilorg/blob/master/templates/code/voting.py',
+            'output': render_template('code/voting.out'),
+        },
+    ]
+    return render_template('index.html', examples=examples)
 
 
 if __name__ == '__main__':
