860fddf91c22126a5aadd4b1418222edd70a57da
Steve Phillips <steve@tryingtobeawesome.com>
2016-01-08 11:04:23 -0800

    Added Example #4: names_runner.py (refers_to_theory_runner function)

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ def index():
             'embed_url': 'github.com/elimisteve/exphilorg/blob/master/templates/code/voting.py',
             'output': render_template('code/voting.out'),
         },
+        {
+            'title': 'names_runner.py',
+            'embed_url': 'github.com/elimisteve/exphilorg/blob/master/templates/code/names_runner.py',
+            'output': render_template('code/names_runner.out'),
+        },
     ]
     return render_template('index.html', examples=examples)
 
